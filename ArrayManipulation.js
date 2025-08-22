09a7b9ef34abde70814082aa979a1799f098651d
rubyarellano <rubyorbasearellano@gmail.com>
2025-08-22 15:16:28 +0800

    Create ArrayManipulation.js

---
+++ ArrayManipulation.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function arrayManipulation(n, queries) {
+    // Write your code here
+    let arr = new Array(n + 2).fill(0);
+
+    // Apply range updates
+    for (let i = 0; i < queries.length; i++) {
+        let a = queries[i][0];
+        let b = queries[i][1];
+        let k = queries[i][2];
+
+        arr[a] += k;
+        arr[b + 1] -= k;
+    }
+
+    // Compute prefix sum and track max
+    let max = 0;
+    let current = 0;
+
+    for (let i = 1; i <= n; i++) {
+        current += arr[i];
+        if (current > max) {
+            max = current;
+        }
+    }
+
+    return max;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = arrayManipulation(n, queries);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
